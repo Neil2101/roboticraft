f3c0cff20051b23de69f912ee22f77b5acea953f
Mintysharky <66876298+Neil2101@users.noreply.github.com>
2022-12-20 23:03:27 +0000

    broke website but fixed everything else

--- keep_alive.py
+++ keep_alive.py
@@ -1,20 +1,20 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/home')
-def home():
-   return render_template('Home.html')
-@app.route('/amogus_sussy')
-def Amogus_sussy():
-   return render_template('Amogus_sussy.html')
-@app.route('/donations')
-def Donations():
-   return render_template('Donations.html')
-@app.route('/sponsors')
-def Sponsors():
-   return render_template('Sponsors.html')
-@app.route('/special_thanks')
-def Special_thanks():
-   return render_template('special_thanks.html')
-
-app.run(host="0.0.0.0", port=5000)
+def keep_alive():
+  from flask import Flask, render_template
+  app = Flask(__name__)
+  
+  @app.route('/home')
+  def home():
+     return render_template('Home.html')
+  @app.route('/amogus_sussy')
+  def Amogus_sussy():
+     return render_template('Amogus_sussy.html')
+  @app.route('/donations')
+  def Donations():
+     return render_template('Donations.html')
+  @app.route('/sponsors')
+  def Sponsors():
+     return render_template('Sponsors.html')
+  @app.route('/special_thanks')
+  def Special_thanks():
+     return render_template('special_thanks.html')
+  app.run(host="0.0.0.0", port=5000)
--- main.py
+++ main.py
@@ -2,14 +2,9 @@
 import os
 import discord
 import random
-import math as rmath
-import cmath
-from time import sleep as wait
 from discord.ui import Button, View
-from threading import Thread
 from keep_alive import keep_alive
 global equationlist
-from replit import db
 import discord.ext.commands
 #variables
 #equationlist is used for the calculator app to store a equation
@@ -23,54 +18,67 @@ max_chance_to_hail = 10
 amt_msg_to_hail = random.randint(1, max_chance_to_hail - 1)
 #intents
 intents = discord.Intents.all()
-bot = discord.Bot(guild_ids=['979939493681438751'],intents=intents)
+bot = discord.Bot(guild_ids=['979939493681438751'], intents=intents)
+
 
 #__init__
 @bot.event
 async def on_ready():
   print(f"We have logged in as {bot.user}")
+
+
 #usless code can't test
 @bot.event
 async def on_member_join(member):
   await member.channel.reply("Hello {}!".format(member))
- 
+
+
 #a link to sussy amogus website
 @bot.slash_command(description="Get the Link to donate!!!!")
 async def linkylink(ctx):
-  embed = discord.Embed(title="Robotibot",description='Donation Link',color=0x42f55d)
-  embed.add_field(name='Donate',value="[**Here**](https://roboticraft.nanmuhongye.repl.co/home)")
+  embed = discord.Embed(title="Robotibot",
+                        description='Donation Link',
+                        color=0x42f55d)
+  embed.add_field(
+    name='Donate',
+    value="[**Here**](https://roboticraft.nanmuhongye.repl.co/home)")
   await ctx.respond(embed=embed)
-  
+
+
 #for playing pong with the bot
-@bot.command(name="ping",description="find the ping of the bot")
+@bot.command(name="ping", description="find the ping of the bot")
 async def latency(ctx):
   await ctx.respond(f'🏓 Pong {round(bot.latency * 1000)} ms!')
-  
+
+
 #haha screw you your getting rickrolled
-@bot.command(description="Sings Rick Asley's Never gonna give you up with you (suggested by Tanda2e5#9606)")
+@bot.command(
+  description=
+  "Sings Rick Asley's Never gonna give you up with you (suggested by Tanda2e5#9606)"
+)
 async def never(ctx):
   global n
-  if n==1:
+  if n == 1:
     await ctx.respond('Never gonna give you up')
-  elif n==2:
+  elif n == 2:
     await ctx.respond('Never gonna let you down')
-  elif n==3:
+  elif n == 3:
     await ctx.respond('Never gonna run around and desert you')
-  elif n==4:
+  elif n == 4:
     await ctx.respond('Never gonna make you cry')
-  elif n==5:
+  elif n == 5:
     await ctx.respond('Never gonna say goodbye')
-  elif n==6:
+  elif n == 6:
     await ctx.respond('Never gonna tell a lie and hurt you')
-  elif n ==7:
+  elif n == 7:
     await ctx.respond('Please praise me, I barely get any money for my singing talent')
-  n = 0
+    n = 0
   n += 1
- 
- #broken calculator... welp
+
+
+#broken calculator... welp
 async def is_owner(ctx):
   return ctx.author.id == 725875148938412104 or ctx.author.id == 560446547989626920
- 
 @bot.command(description="Grants someone the suggestor role... purely cosmetic")
 @discord.ext.commands.check(is_owner)
 async def grant_suggestor(ctx, user: discord.Member):
@@ -78,8 +86,9 @@ async def grant_suggestor(ctx, user: discord.Member):
   role = discord.utils.get(ctx.guild.roles, name="Bot suggestors")
   await member.add_roles(role)
   await ctx.respond("Done!")
- 
- 
+
+
+
 def calculator(Input_List):
   try:
     global numvar1
@@ -119,17 +128,18 @@ def calculator(Input_List):
   except Exception as e:
     print(e)
     return "No Answer, If you did a valid calculation, please contact the owner of this bot at Mintysharky#1496 and send him the dev stuff | DEV STUFF: " + str(
- e)
- 
- #just automate the making of buttons
+      e)
+
+
+#just automate the making of buttons
 class CalculatorButton(Button):
   try:
     global equation
     equation = "0"
-  
+
     def __init__(self, number, style=discord.ButtonStyle.gray):
       super().__init__(label=number, style=style)
-     
+
     async def callback(self, interaction):
       global equation
       if not self.label == "Done":
@@ -147,10 +157,11 @@ class CalculatorButton(Button):
         await interaction.message.edit(content=calculator(equationlist))
   except:
     pass
- 
- 
- #the actual calculator
-@bot.command(description="Calculates something (+,-,/,*) One instance at a time[BETA]")
+
+
+#the actual calculator
+@bot.command(
+  description="Calculates something (+,-,/,*) One instance at a time[BETA]")
 async def calculate(ctx):
   calcbuttons = []
   view = View()
@@ -165,12 +176,11 @@ async def calculate(ctx):
   for i in calcbuttons:
     view.add_item(i)
   await ctx.respond("0", view=view)
- 
- #runs the bot
+
+#runs the bot
 Token = os.environ['TOKEN']
 bot.run(Token)
 keep_alive()
-
 
 #haha screw you you aren't getting the botmaker role haha
 """@bot.slash_command(description="get the bot creator role")
